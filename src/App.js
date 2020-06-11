2784cfb68f11b759749fdf5de97ab21f01fe7c96
Maya Novarini <mayanovarini@Mayas-MacBook-Pro.local>
2020-06-10 23:49:45 -0700

    refactor updateTodo and add effect

--- src/App.js
+++ src/App.js
@@ -1,14 +1,47 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import Header from './Header';
 import TodoList from './TodoList';
 import Filters from './Filters';
 import TodoInput from './TodoInput';
+import Gif from './Gif';
 import shortid from 'shortid';
+import { giphyKey } from './config';
 import './App.css';
 
 const App = () => {
   const [ todos, setTodos ] = useState([]);
   const [ filter, setFilter ] = useState("all");
+  const [ gifImage, setGifImage ] = useState(null);
+
+  const numFinished = todos.filter(todo => todo.finished).length;
+  const numUnfinished = todos.filter(todo => !todo.finished).length;
+  const filteredTodos = todos.filter(todo => {
+    switch (filter) {
+      case 'finished':
+        return todo.finished;
+      case 'unfinished':
+        return !todo.finished;
+      default:
+        return true;
+    }
+  });
+
+  const keyword = (filter === 'finished' && numFinished > 0) 
+    ? 'congrats' 
+    : 'sleepy dog';
+  const gifData = "https://api.giphy.com/v1/gifs/random?" +
+    `api_key=${giphyKey}&` +
+    `tag=${keyword}&limit=1&offset=0&rating=G&lang=en`;
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch(gifData);
+      return await response.json();
+    }
+    fetchData().then(data => {
+      setGifImage(data.data.images.original.url);
+    });
+  }, [filter, gifData]);
 
   const handleNewTodo = (todo) => {
     setTodos(todos => [...todos, {
@@ -22,8 +55,11 @@ const App = () => {
     const todoIndex = todos.findIndex(todo => todo.id === id);
     // copy the todo object and merge it with the new property object
     const newTodo = {...todos[todoIndex], ...prop};
-    todos[todoIndex] = newTodo;
-    return [...todos];
+    return [
+      ...todos.slice(0, todoIndex),
+      newTodo,
+      ...todos.slice(todoIndex + 1)
+    ];
   };
 
   const handleChangeTodo = (id, text) => {
@@ -37,23 +73,11 @@ const App = () => {
   const handleDeleteTodo = (id) => {
     setTodos(todos => todos.filter(todo => todo.id !== id));
   };
-
-  const numFinished = todos.filter(todo => todo.finished).length;
-  const numUnfinished = todos.filter(todo => !todo.finished).length;
-  const filteredTodos = todos.filter(todo => {
-    switch (filter) {
-      case 'all':
-        return true;
-      case 'finished':
-        return todo.finished;
-      case 'unfinished':
-        return !todo.finished;
-    }
-  })
   
   return (
     <div className="App">
       <Header count={numUnfinished} />
+      <Gif image={gifImage} />
       <Filters
         {...{
           numFinished,
@@ -62,6 +86,7 @@ const App = () => {
           onSwitch: setFilter
         }}
       />
+      <TodoInput onNewTodo={handleNewTodo} />
       <TodoList
         {...{
           todos: filteredTodos,
@@ -70,7 +95,6 @@ const App = () => {
           onDeleteTodo: handleDeleteTodo
         }}        
       />
-      <TodoInput onNewTodo={handleNewTodo} />
     </div>
   );
 }
